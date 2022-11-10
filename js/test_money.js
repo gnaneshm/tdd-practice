2ed25fcbf65fa7a51316ccdd948b4aa47513a7c2
gnaneshm <gnaneshm95@gmail.com>
2022-11-10 17:14:45 +0530

    added divide function to class and wrote test cases

--- js/test_money.js
+++ js/test_money.js
@@ -1,12 +1,33 @@
 const assert = require("assert");
-class Dollar {
-  constructor(amount) {
+
+// let fiver = new Dollar(5);
+// let tenner = fiver.times(2);
+// assert.strictEqual(tenner.amount, 10);
+
+class Money {
+  constructor(amount, currency) {
     this.amount = amount;
+    this.currency = currency;
   }
   times(multiplier) {
-    return new Dollar(this.amount * multiplier);
+    return new Money(this.amount * multiplier, this.currency);
+  }
+  divide(divisor) {
+    return new Money(this.amount / divisor, this.currency);
   }
 }
-let fiver = new Dollar(5);
+
+let fiver = new Money(5, "USD");
 let tenner = fiver.times(2);
-assert.strictEqual(tenner.amount, 10);
+let expectedMoneyAfterDivision = new Money(10, "USD");
+assert.deepStrictEqual(tenner, expectedMoneyAfterDivision);
+
+let tenEuros = new Money(10, "EUR");
+let twentyEuros = tenEuros.times(2);
+expectedMoneyAfterDivision = new Money(20, "EUR");
+assert.deepStrictEqual(twentyEuros, expectedMoneyAfterDivision);
+
+let originalMoney = new Money(4002, "KRW");
+let actualMoneyAfterDivision = originalMoney.divide(4);
+expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
