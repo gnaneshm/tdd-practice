98c0843b589304d143497e0d89f8a3c32612ceb5
gnaneshm <gnaneshm95@gmail.com>
2022-11-15 09:07:12 +0530

    feat: conversion of Money from EUR to USD

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,9 +6,16 @@ export class Portfolio {
   add(...moneys) {
     this.moneys = this.moneys.concat(moneys);
   }
+  convert(money, currency) {
+    let eurToUsd = 1.2;
+    if (money.currency === currency) {
+      return money.amount;
+    }
+    return money.amount * eurToUsd;
+  }
   evaluate(currency) {
     let total = this.moneys.reduce((sum, money) => {
-      return sum + money.amount;
+      return sum + this.convert(money, currency);
     }, 0);
     return new Money(total, currency);
   }
