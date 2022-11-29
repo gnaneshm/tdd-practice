573c63d4b4e26249d6feb7d8e5843676f045ad66
gnaneshm <gnaneshm95@gmail.com>
2022-11-29 14:06:40 +0530

    test: verified behavior when modifying an existing exchange rate

--- js/test_money.js
+++ js/test_money.js
@@ -4,7 +4,7 @@ import { Portfolio } from "./portfolio.js";
 import { Bank } from "./bank.js";
 
 class MoneyTest {
-  constructor() {
+  setUp() {
     this.bank = new Bank();
     this.bank.addExchangeRate("EUR", "USD", 1.2);
     this.bank.addExchangeRate("USD", "KRW", 1100);
@@ -64,12 +64,28 @@ class MoneyTest {
       expectedError
     );
   }
-  testConversion() {
-    let bank = new Bank();
-    bank.addExchangeRate("EUR", "USD", 1.2);
+  testConversionWithDifferentRatesBetweenTwoCurrencies() {
     let tenEuros = new Money(10, "EUR");
-    assert.deepStrictEqual(bank.convert(tenEuros, "USD"), new Money(12, "USD"));
+    assert.deepStrictEqual(
+      this.bank.convert(tenEuros, "USD"),
+      new Money(12, "USD")
+    );
+
+    this.bank.addExchangeRate("EUR", "USD", 1.3);
+    assert.deepStrictEqual(
+      this.bank.convert(tenEuros, "USD"),
+      new Money(13, "USD")
+    );
   }
+  // below test case is not needed now
+
+  // testWhatIsTheConversionRateFromEURToUSD() {
+  //   let tenEuros = new Money(10, "EUR");
+  //   assert.deepStrictEqual(
+  //     this.bank.convert(tenEuros, "USD"),
+  //     new Money(12, "USD")
+  //   );
+  // }
   testConversionWithMissingExchangeRate() {
     let bank = new Bank();
     let tenEuros = new Money(10, "EUR");
@@ -77,6 +93,13 @@ class MoneyTest {
     assert.throws(function () {
       bank.convert(tenEuros, "Kalganid");
     }, expectedError);
+  }
+  randomizeTestOrder(testMethods) {
+    for (let i = testMethods.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [testMethods[i], testMethods[j]] = [testMethods[j], testMethods[i]];
+    }
+    return testMethods;
   }
   getAllTestMethods() {
     let moneyPrototype = MoneyTest.prototype;
@@ -92,6 +115,7 @@ class MoneyTest {
       console.log("Running :  %s()", m);
       let method = Reflect.get(this, m);
       try {
+        this.setUp();
         Reflect.apply(method, this, []);
       } catch (e) {
         if (e instanceof assert.AssertionError) {
