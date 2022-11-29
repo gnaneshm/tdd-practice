8161dd25ef726105e9adc349449e6dc27dcc8323
gnaneshm <gnaneshm95@gmail.com>
2022-11-29 13:17:14 +0530

    feat: added Bank; refactored Portfolio to use Bank

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,29 +6,32 @@ export class Portfolio {
   add(...moneys) {
     this.moneys = this.moneys.concat(moneys);
   }
-  convert(money, currency) {
-    let exchangeRates = new Map();
-    exchangeRates.set("EUR->USD", 1.2);
-    exchangeRates.set("USD->KRW", 1100);
-    if (money.currency === currency) {
-      return money.amount;
-    }
-    let key = money.currency + "->" + currency;
-    let rate = exchangeRates.get(key);
-    if (rate === undefined) {
-      return undefined;
-    }
-    return money.amount * rate;
-  }
-  evaluate(currency) {
+  // since we have convert method in Bank class, we dont need convert in Portfolio class
+
+  // convert(money, currency) {
+  //   let exchangeRates = new Map();
+  //   exchangeRates.set("EUR->USD", 1.2);
+  //   exchangeRates.set("USD->KRW", 1100);
+  //   if (money.currency === currency) {
+  //     return money.amount;
+  //   }
+  //   let key = money.currency + "->" + currency;
+  //   let rate = exchangeRates.get(key);
+  //   if (rate === undefined) {
+  //     return undefined;
+  //   }
+  //   return money.amount * rate;
+  // }
+  evaluate(bank, currency) {
     let failures = [];
     let total = this.moneys.reduce((sum, money) => {
-      let convertedAmount = this.convert(money, currency);
-      if (convertedAmount === undefined) {
-        failures.push(money.currency + "->" + currency);
+      try {
+        let convertedMoney = bank.convert(money, currency);
+        return sum + convertedMoney.amount;
+      } catch (error) {
+        failures.push(error.message);
         return sum;
       }
-      return sum + convertedAmount;
     }, 0);
     if (!failures.length) {
       return new Money(total, currency);
